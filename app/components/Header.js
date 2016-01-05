4ad72a98bb31cec8c86116845091e3b3d41e919d
Daniel Ayala <dmayala001@gmail.com>
2016-01-04 21:42:09 -0800

    Add Admin navitems to Header

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Link} from 'react-router';
 import {Navbar, Nav, DropdownButton, MenuItem, NavItem} from 'react-bootstrap';
+import {LinkContainer} from 'react-router-bootstrap';
 
 if (process.env.BROWSER) {
   require('stylesheets/components/_Header');
@@ -11,10 +12,50 @@ class Header extends React.Component {
   static contextTypes = { 
     flux: React.PropTypes.object.isRequired,
   }
+
+  state = this._getLoginState();
+
+  _getLoginState() {
+    let store = this.context.flux.getStore('login');
+    let loginState = store.getState();
+    
+    return {
+      user: loginState._user,
+      jwt: loginState._jwt
+    };
+  }
+
+  _onChange = () => {
+    this.setState(this._getLoginState());
+  }
+
+  componentDidMount() {
+    this.context.flux
+              .getStore('login')
+              .listen(this._onChange);
+  }
+
+  componentWillUnmount() {
+    this.context.flux
+              .getStore('login')
+              .unlisten(this._onChange);
+  }
   
   _onLogout = (e) => {
     e.preventDefault();
     this.context.flux.getActions('login').logoutUser();
+  }
+
+  _adminNav = () => {
+      return [
+        (<LinkContainer key={1} to="dashboard">
+          <NavItem eventKey={1}>Add Survey</NavItem>
+        </LinkContainer>),
+        (<LinkContainer key={2} to="results">
+          <NavItem eventKey={2}>View Results</NavItem>
+        </LinkContainer>),
+        (<NavItem key={3} eventKey={3} onClick={this._onLogout}>Log Out</NavItem>),
+      ];
   }
 
   render() {
@@ -30,8 +71,10 @@ class Header extends React.Component {
           <Nav navbar>
           </Nav>
           <Nav navbar pullRight>
-            <NavItem eventKey={1} href="#">Log In</NavItem>
-            <NavItem eventKey={1} href="#" onClick={ this._onLogout }>Log Out</NavItem>
+           { !this.state.user ? 
+              <NavItem eventKey={1} href="dashboard">Log In</NavItem> :
+              this._adminNav()
+           }
           </Nav>
         </Navbar.Collapse>
       </Navbar>
--- app/routes.js
+++ app/routes.js
@@ -4,7 +4,6 @@ import { IndexRoute, Route } from 'react-router';
 import { isConnected } from 'utils/routesHooks';
 
 import MainApp from 'components/MainApp';
-import Home from 'components/Home';
 import Survey from 'components/Survey';
 import AddSurvey from 'components/AddSurvey';
 import SurveyResults from 'components/SurveyResults';
@@ -13,12 +12,13 @@ import Login from 'components/Login';
 export default function (flux) {
   return (
     <Route component={MainApp}>
-      <Route path="/" component={Home} />
+      <Route path="/" component={Survey} />
       <Route path="dashboard" onEnter={ isConnected(flux) }>
         <IndexRoute component={AddSurvey} />
         <Route path="results" component={SurveyResults} />
       </Route>
       <Route path="survey" component={Survey} />
+      <Route path="results" component={SurveyResults} />
       <Route path="login" component={Login} />
     </Route>
   );
